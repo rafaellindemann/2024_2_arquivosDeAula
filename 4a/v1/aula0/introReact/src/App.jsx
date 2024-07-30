ba37c41e381e9414f020d5a242e8d14d4628e505
Professor SESI/SENAI <Professor@SN-371157.edu.fiesc.com.br>
2024-07-30 17:33:35 -0300

    add HelloWorld react

---
+++ 4a/v1/aula0/introReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Conteudo from './components/Conteudo'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className='app-container'>
+      <Navbar />
+      <Conteudo />
+    </div>
+  )
+}
+
+export default App
