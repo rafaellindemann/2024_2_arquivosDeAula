b31039e33722f5363c88a99c1109cb9a23d58ded
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-08-05 20:56:45 -0300

    Att Main.jsx com as particularidades da versão

--- 4a/comuns/layoutHamburger/src/components/Main.jsx
+++ 4a/comuns/layoutHamburger/src/components/Main.jsx
@@ -5,7 +5,14 @@ function Main() {
   return (
     <main className="main">
         <p>Conteúdo principal que vai crescer automaticamente...</p>
-        <p>Estamos testando o hamburger</p>
+        <p>Funcionalidades deste demo:</p>
+        <ul>
+          <li>Menu hamburger automático via @media para telas pequenas;</li>
+          <li>Background de padrão svg da haikei;</li>
+          <li>Background de gradiente do coolors</li>
+        </ul>
+
+        <p>Este mini demo serve como um ótimo template para aplicar navegações.</p>
 
         {/* Adicione mais conteúdo aqui */}
     </main>
