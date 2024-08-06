3585e969bb1358316eaf722beb9a32f276137cdc
Julio Cezar <julio.rutke@edu.sc.senai.br>
2024-08-06 20:36:50 -0300

    att aula00 n1

--- 4a/n1/aula00/src/components/Conteudo/index.jsx
+++ 4a/n1/aula00/src/components/Conteudo/index.jsx
@@ -3,6 +3,7 @@ function Conteudo() {
   return (
     <div className="conteudo-container">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quasi inventore et sunt autem excepturi. Architecto, suscipit iste et dignissimos unde alias eveniet sapiente, mollitia ex ut reprehenderit, eius ipsam!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam nisi praesentium rem consequatur, hic a optio ullam iste aliquam officiis eos qui omnis provident, similique laborum maxime autem error tempore.</p>
     </div>
   )
 }
---
+++ 4a/n1/aula00/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Conteudo from "../components/Conteudo"
+import Header from "../components/Header"
+
+function Home() {
+  return (
+    <div className="home-container">
+      <Header />
+      <Conteudo />
+    </div>
+  )
+}
+
+export default Home
