ac76ffab2d871ada592f9a2135e84f71d356f512
Julio Cezar <julio.rutke@edu.sc.senai.br>
2024-08-06 22:18:47 -0300

    Add react aula00 n1

--- 4a/n1/aula00/src/components/Conteudo/index.jsx
+++ 4a/n1/aula00/src/components/Conteudo/index.jsx
@@ -2,8 +2,10 @@ import './index.css'
 function Conteudo() {
   return (
     <div className="conteudo-container">
+      <img className='flor-rosa' src="./images/flor.png" alt="foto de uma rosa vermelha" />
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quasi inventore et sunt autem excepturi. Architecto, suscipit iste et dignissimos unde alias eveniet sapiente, mollitia ex ut reprehenderit, eius ipsam!</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam nisi praesentium rem consequatur, hic a optio ullam iste aliquam officiis eos qui omnis provident, similique laborum maxime autem error tempore.</p>
+
+
     </div>
   )
 }
