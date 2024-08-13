a275e5321c64fa77c79214c8581a9430d5ce09c4
Julio Cezar <julio.rutke@edu.sc.senai.br>
2024-08-13 20:15:17 -0300

    add aula01 4aFn1

---
+++ 4a/n1/aula01/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  const [contador,setContador] = useState(50)
+
+  let cont = 99;
+
+  function tratarClique(){
+    cont++;
+    console.log(cont);
+  }
+  function incrementarContador(){
+    setContador(contador + 1)
+  }
+  return (
+    <>
+      <h1>titulo</h1>
+      <button onClick={tratarClique}>Clique</button>
+      <div>
+        {cont}
+      </div>
+
+      <button onClick={incrementarContador}>Incrementar</button>
+      <div>
+        {contador}
+      </div>
+    </>
+  )
+}
+
+export default App
