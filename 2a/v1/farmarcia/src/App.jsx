54e050f3aae3a8fbe67343baf4a94e768ea13520
rafaellindemann <rafaelld@gmail.com>
2024-08-26 17:31:53 -0300

    add wip farmarcia v1

---
+++ 2a/v1/farmarcia/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='app-container'>
+      <h1 className='teste'>Farmárcia</h1>
+    </div>
+    
+  )
+}
+
+export default App
