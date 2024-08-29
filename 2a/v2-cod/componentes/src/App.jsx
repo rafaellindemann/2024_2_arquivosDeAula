909b95b222b01fcc3b9445931222f40f54ee8b1f
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-08-29 17:20:45 -0300

    add criação de componentes e mais umas coisas legais

---
+++ 2a/v2-cod/componentes/src/App.jsx
@@ -0,0 +1,40 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Atalhos do VS Code</h1>
+      <h2>Coisas legais para facilitar tua vida</h2>
+      <p>
+        Comando para o emmet da ul: ul>li*6
+      </p>
+      <ul>
+        <li>Atalho: alt + z para quebra automática de linha.</li>
+        <li>Atalho: alt + clique esquerdo para adicionar novos cursores na tela</li>
+        <li>Atalho: ctrl+shift+k para excluir uma linha inteira</li>
+        <li>Atalho: ctrl+; para comentar/descomentar</li>
+        <li>Atalho: windows+. abre teclado de emojis</li>
+        <li>Atalho: alt+↕ para mover uma linha/bloco </li>
+        <li>Atalho: alt+shift+↕ copia a linha atual ou bloco selecionado</li>
+        <li>Atalho: ctrl+j ou ctrl+' abre/fecha o terminal</li>
+      </ul>
+      <p>Para esfregar vários cursores pela tela (digitar várias coisas ao mesmo tempo): Clica com a rodinha e esfrega ela pela tela.</p>
+
+      <p>Extensões:</p>
+      <ul>
+        <li>ES7 React/Redux/GraphQL...</li>
+        <li>Auto rename tag</li>
+        <li>Color highlight</li>
+        <li>Power Mode</li>
+        <li>VSCode Pets</li>
+      </ul>
+      
+
+
+    </>
+  )
+}
+
+export default App
