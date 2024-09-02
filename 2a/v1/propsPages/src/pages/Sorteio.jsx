d3028efea997bd3c72f37e2a014664c50d1eba41
rafaellindemann <rafaelld@gmail.com>
2024-09-02 14:54:50 -0300

    add useState v1

--- 2a/v1/propsPages/src/pages/Sorteio.jsx
+++ 2a/v1/propsPages/src/pages/Sorteio.jsx
@@ -5,21 +5,28 @@ import Titulo from '../components/Titulo'
 
 function Sorteio() {
   const [numero, setNumero] = useState()
+  // const [dado, setDado] = useState(1)
 
     function sortear(){
         let n = Math.floor(Math.random()*100)
         //alert(n)
         setNumero(n)
     }
+    function aumentar(){
+      setNumero(numero + 1)
+    }
+    function diminuir(){
+      setNumero(numero - 1)
+    }
   return (
     <div className='sorteio-container'>
       <Titulo texto={"Sorteio"} emoji={"🎲"}/>
 
-      <div className="classeDela"></div>
-
+      <button onClick={aumentar}>+</button>
       <div className="numero">
         {numero}
       </div>
+      <button onClick={diminuir}>-</button>
       <button className='btn-sortear' onClick={sortear}>Sortear número</button>
     </div>
   )
