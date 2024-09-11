cc294260aa4e739d234c4c2cbbe97635a24bf531
rafaellindemann <rafaelld@gmail.com>
2024-09-11 16:01:48 -0300

    add navState 2Av1

---
+++ 2a/v1/paginas-useState/src/pages/Contato.jsx
@@ -0,0 +1,11 @@
+
+function Contato() {
+  return (
+    <div>
+        <h1>Contatos</h1>
+        <p>Para falar conosco, entrem em contato. Vamos marcar...</p>
+    </div>
+  )
+}
+
+export default Contato
---
+++ 2a/v1/paginas-useState/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+function Home() {
+  return (
+    <div>
+      <h1>Eu sou uma Home Page de um site muito bonito.</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga sequi impedit dolorem nesciunt? At, sed! Molestias rerum cum inventore ullam a fugit laudantium exercitationem, itaque nam consectetur sequi alias quae!</p>
+      <h2>loremzinho</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti repellendus dolores voluptates. Similique aspernatur neque earum qui quisquam, autem, voluptatibus eum ipsum possimus, consequuntur animi. Facere quas porro quibusdam distinctio!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, laudantium culpa quisquam adipisci nulla labore reprehenderit, tempore cupiditate nostrum quo saepe earum nihil facilis eaque omnis, ratione molestiae soluta sint!</p>
+    </div>
+  )
+}
+
+export default Home
