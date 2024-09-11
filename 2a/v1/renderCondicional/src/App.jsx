e1515438b17aafec1e207bd15894738462994c6e
rafaellindemann <rafaelld@gmail.com>
2024-09-11 15:34:21 -0300

    add exemplos render condicional 2aV1

---
+++ 2a/v1/renderCondicional/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import { useState } from 'react';
+import OlaUsuario from './components/OlaUsuario';
+import AreaParaAdultos from './components/AreaParaAdultos';
+import Login from './components/Login';
+
+function App() {
+  const [logado, setLogado] = useState(true);
+  const [idade, setIdade] = useState(0)
+  function logar(){
+    setLogado(true)
+  }
+  return (
+    <>
+      <div className="render-container">
+        <button onClick={logar}>Logar</button>
+        <button onClick={ () => {setLogado(false)} }>10Logar</button>
+        <button onClick={ () => setLogado(!logado) }>Alternar</button>
+        { logado && <OlaUsuario /> }
+      </div>
+      <div className="render-container">
+        <button onClick={() => setIdade(idade - 1)}>-</button>
+        {idade}
+        <button onClick={() => setIdade(idade + 1)}>+</button>
+        { idade>=18 && <AreaParaAdultos /> }
+      </div>
+      <div className="render-container">
+        { logado==true ? <OlaUsuario /> : <Login /> }
+      </div>
+
+    </>
+  )
+}
+
+export default App
