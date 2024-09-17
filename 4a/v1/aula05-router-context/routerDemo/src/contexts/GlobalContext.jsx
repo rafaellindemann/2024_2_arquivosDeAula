8baae67598b540c5ef7dea960dcc8635e9109d18
rafaellindemann <rafaelld@gmail.com>
2024-09-17 16:51:33 -0300

    add demo context com array/state

--- 4a/v1/aula05-router-context/routerDemo/src/contexts/GlobalContext.jsx
+++ 4a/v1/aula05-router-context/routerDemo/src/contexts/GlobalContext.jsx
@@ -1,14 +1,31 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext()
 
 export const GlobalContextProvider = ({children}) => {
     // informações globais
     let usuarioLogado = 'Gill Bates'
+    const [usuarios, setUsuarios] = useState([
+        {
+            id: Date.now(),
+            nome: 'ET Bilu',
+            email: 'busquem@conhecimento.com.br'
+        },
+        {
+            id: Date.now()+1,
+            nome: 'Mano Juca',
+            email: 'mj-vidaloka@email.com'
+        },
+        {
+            id: Date.now()+2,
+            nome: 'Gill Bates',
+            email: 'gill@sicromoft.com'
+        },
+    ])
     
     // /informações globais
     return (
-        <GlobalContext.Provider value={{usuarioLogado}}>
+        <GlobalContext.Provider value={{usuarioLogado, usuarios, setUsuarios}}>
             {children}
         </GlobalContext.Provider>
     )
--- 4a/v1/aula05-router-context/routerDemo/src/pages/Home.jsx
+++ 4a/v1/aula05-router-context/routerDemo/src/pages/Home.jsx
@@ -3,7 +3,15 @@ import {useContext} from 'react'
 import { GlobalContext } from '../contexts/GlobalContext'
 
 function Home() {
-    const {usuarioLogado} = useContext(GlobalContext)
+    const {usuarioLogado, usuarios, setUsuarios} = useContext(GlobalContext)
+    function adicionarUsuario(){
+        let usuarioNovo = {
+            id: Date.now(),
+            nome: "Capitão Ganso",
+            email: "capitao@juliaroger.com"
+        }
+        setUsuarios([...usuarios,usuarioNovo])
+    }
   return (
     <div>
         <Navbar />
@@ -11,6 +19,21 @@ function Home() {
        <p>
         Olá {usuarioLogado}
         </p> 
+        <div>
+            {
+                usuarios.map((usuario) => (
+                    <div key={usuario.id}>
+                        <p>Nome: {usuario.nome}</p>
+                        <p>E-mail: {usuario.email}</p>
+                        <p>{usuario.id}</p>
+                    </div>
+                ))
+            }
+        </div>
+        <div>
+
+            <button onClick={adicionarUsuario}>Adicionar usuário</button>
+        </div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores consectetur veritatis culpa, adipisci voluptatibus doloribus officia at. A nihil odit quasi ea incidunt repellendus neque architecto, eum, debitis temporibus tempore.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsum quo placeat, harum repellendus, neque optio exercitationem perspiciatis quod itaque eos illum. Voluptate repellat distinctio doloremque praesentium aperiam? Repellat, dolorum.</p>
     </div>
