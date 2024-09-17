89ab9d3833e8ad0d91dd99c65381c5ad6e754b2c
rafaellindemann <rafaelld@gmail.com>
2024-09-17 20:11:26 -0300

    add temp router

--- 4a/n1/aula04-router-context/router-context/src/components/Navbar.jsx
+++ 4a/n1/aula04-router-context/router-context/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
-
+import './Navbar.css'
 function Navbar() {
   return (
-    <nav>
+    <nav className="navbar">
         <Link to="/">Home</Link>
         <Link to="/contato">Contato</Link>
         <Link to="/generica">Genérica</Link>
+        <Link to="/final">Final</Link>
     </nav>
   )
 }
