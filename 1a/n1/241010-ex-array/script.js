d1e38876592871f5a1326f1e754ce2c8a88cd73c
rafaellindemann <rafaelld@gmail.com>
2024-10-10 22:01:44 -0300

    adicionadas divagações sobre arrays e outras coisas

---
+++ 1a/n1/241010-ex-array/script.js
@@ -0,0 +1,21 @@
+let numeros = []
+
+function executar83(){
+    // alert("funcionando")
+    let numero
+
+    numero = Math.ceil(Math.random()*6)
+
+    numeros.push(numero)
+
+    console.log(numeros);
+}
+
+
+// 23.9 >> 23 floor
+// 23.9 >> 24 ceil
+// 23.9 >> 24 round
+
+// 23.2 >> 23 floor
+// 23.2 >> 24 ceil
+// 23.2 >> 23 round
